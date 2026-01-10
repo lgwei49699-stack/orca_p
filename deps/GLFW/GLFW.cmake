4d0a53be30a9e872f212861ded00ecad7a40a75f
liwei <ligw@gongfudou.com>
2026-01-11 01:41:58 +0800

    feat:完全禁用 X11 和 Wayland

--- deps/GLFW/GLFW.cmake
+++ deps/GLFW/GLFW.cmake
@@ -7,24 +7,26 @@ else()
 endif()
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    set(_glfw_use_wayland "-DGLFW_USE_WAYLAND=OFF")
+    set(_glfw_disable_x11 "-DGLFW_BUILD_X11=OFF")
+    set(_glfw_disable_wayland "-DGLFW_BUILD_WAYLAND=OFF")
     set(_glfw_use_osmesa "-DGLFW_BUILD_OSMESA=ON")
 else()
-    set(_glfw_use_wayland "-DGLFW_USE_WAYLAND=OFF")
+    set(_glfw_disable_x11 "")
+    set(_glfw_disable_wayland "")
     set(_glfw_use_osmesa "")
 endif()
 
 orcaslicer_add_cmake_project(GLFW
     URL https://github.com/glfw/glfw/archive/refs/tags/3.3.7.zip
     URL_HASH SHA256=e02d956935e5b9fb4abf90e2c2e07c9a0526d7eacae8ee5353484c69a2a76cd0
-    #DEPENDS dep_Boost
     CMAKE_ARGS
         -DBUILD_SHARED_LIBS=${_build_shared} 
         -DGLFW_BUILD_DOCS=OFF
         -DGLFW_BUILD_EXAMPLES=OFF
-	-DGLFW_BUILD_TESTS=OFF
-	${_glfw_use_wayland}
-	${_glfw_use_osmesa}
+        -DGLFW_BUILD_TESTS=OFF
+        ${_glfw_disable_x11}
+        ${_glfw_disable_wayland}
+        ${_glfw_use_osmesa}
 )
 
 if (MSVC)
